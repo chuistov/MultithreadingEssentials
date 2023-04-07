6ad9371e214ae681877ac4323a8de604a0ca3fac
Nikolay Chuistov <chuistov.it@gmail.com>
2023-04-07 23:33:29 +0300

    more Lesson 13

--- src/lesson13_ThreadPool_Future_Callable/ThreadPoolDemo.java
+++ src/lesson13_ThreadPool_Future_Callable/ThreadPoolDemo.java
@@ -1,17 +1,33 @@
 package lesson13_ThreadPool_Future_Callable;
 
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 public class ThreadPoolDemo {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
 
-        Executors.newSingleThreadExecutor();
-        Executors.newFixedThreadPool(5);
-        Executors.newCachedThreadPool();
+        ExecutorService threadPool = Executors.newFixedThreadPool(5);
+        Future<Integer> future = threadPool.submit(() -> {
+            Thread.sleep(2000L);
+            System.out.println("It's a callable");
+            return 1;
+        });
 
-        Executors.newScheduledThreadPool(5);
-        Executors.newWorkStealingPool();
+        System.out.println("Result: " + future.get());
+        threadPool.shutdown();
+        threadPool.awaitTermination(1, TimeUnit.SECONDS);
+
+        System.out.println("main thread");
+
+        ScheduledExecutorService threadPool1 = Executors.newScheduledThreadPool(5);
+        threadPool1.schedule(() -> System.out.println("Hello from scheduled"), 3L, TimeUnit.SECONDS);
+
+        threadPool1.scheduleAtFixedRate(
+                () -> System.out.println("Hi again!"),
+                0L, 3L, TimeUnit.SECONDS
+        );
+
+
 
 
     }
